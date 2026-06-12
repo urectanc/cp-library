1264bfa630a1c4238ac0534ab3832f44b84a3059
urectanc <urectanc@gmail.com>
2026-06-12 22:12:32 +0900

    feat(algebra): add monoid adapters

--- crates/urectanc/algebra/src/lib.rs
+++ crates/urectanc/algebra/src/lib.rs
@@ -36,3 +36,39 @@ impl<M: Monoid> Monoid for Reverse<M> {
         M::op(rhs, lhs)
     }
 }
+
+pub struct Reversible<M: Monoid> {
+    _phantom: std::marker::PhantomData<M>,
+}
+
+impl<M: Monoid> Monoid for Reversible<M> {
+    type Elem = (M::Elem, M::Elem);
+
+    #[inline]
+    fn identity() -> Self::Elem {
+        (M::identity(), M::identity())
+    }
+
+    #[inline]
+    fn op(lhs: &Self::Elem, rhs: &Self::Elem) -> Self::Elem {
+        (M::op(&lhs.0, &rhs.0), M::op(&rhs.1, &lhs.1))
+    }
+}
+
+pub struct MonoidArray<M, const N: usize> {
+    _phantom: std::marker::PhantomData<M>,
+}
+
+impl<M: Monoid, const N: usize> Monoid for MonoidArray<M, N> {
+    type Elem = [M::Elem; N];
+
+    #[inline]
+    fn identity() -> Self::Elem {
+        std::array::from_fn(|_| M::identity())
+    }
+
+    #[inline]
+    fn op(lhs: &Self::Elem, rhs: &Self::Elem) -> Self::Elem {
+        std::array::from_fn(|i| M::op(&lhs[i], &rhs[i]))
+    }
+}
